a9fe0ceb4f83308ffa18ef2cc92e45e32ebd669a
Minh D. Nguyen <ndminh1990@gmail.com>
2020-10-12 22:42:10 -0500

    update server.R to input state tab

--- app/server.R
+++ app/server.R
@@ -70,7 +70,7 @@ shinyServer(function(input, output) {
             # add confirmed case lines
             case_fig <- case_fig %>% add_lines(
                 x = ~date_choices[1:end_date_index], 
-                y = ~log(as.numeric(aggre_cases[input$country,])[1:end_date_index]),
+                y = ~as.numeric(aggre_cases_log[input$country,])[1:end_date_index],
                 line = list(color = 'rgba(67,67,67,1)', width = 2),
                 name = 'Confirmed Cases'
             )
@@ -78,7 +78,7 @@ shinyServer(function(input, output) {
             #add death line 
             case_fig <- case_fig %>% add_lines(
                 x = ~date_choices[1:end_date_index],
-                y = ~log(as.numeric(aggre_death[input$country,])[1:end_date_index]),
+                y = ~as.numeric(aggre_death_log[input$country,])[1:end_date_index],
                 name = 'Death Toll'
             )
             
@@ -93,8 +93,77 @@ shinyServer(function(input, output) {
         return(case_fig)
     })
     
+    # tab panel 2 - State Plots ----------------------------------------
+    # prepare data for plot
+    output$case_overtime_state <- renderPlotly({
+        
+        # determine the row index for subset
+        req(input$log_scale_2)
+        end_date_index_2 <- which(date_choices == input$date_2)
+        
+        # if log scale is not enabled, we will just use cases
+        if (input$log_scale_2 == FALSE) {
+            
+            # render plotly figure
+            case_fig_2 <- plot_ly()
+            
+            # add confirmed case lines
+            case_fig_2 <- case_fig_2 %>% add_lines(
+                x = ~date_choices[1:end_date_index_2], 
+                y = ~as.numeric(aggre_cases[input$country,])[1:end_date_index_2],
+                line = list(color = 'rgba(67,67,67,1)', width = 2),
+                name = 'Confirmed Cases'
+            )
+            
+            # add death line 
+            case_fig_2 <- case_fig_2 %>% add_lines(
+                x = ~date_choices[1:end_date_index_2],
+                y = ~as.numeric(aggre_death[input$country,])[1:end_date_index_2],
+                name = 'Death Toll'
+            )
+            
+            # set the axis for the plot
+            case_fig_2 <- case_fig_2 %>% layout(
+                title = paste0(input$country,'\t','Trend'),
+                xaxis = list(title = 'Date',showgrid = FALSE), 
+                yaxis = list(title = 'Comfirmed Cases/Deaths', showgrid=FALSE)
+            )
+        }
+        
+        # if log scale is enabled, we need to take log of the y values
+        else{
+            
+            # render plotly figure
+            case_fig_2 <- plot_ly()
+            
+            # add confirmed case lines
+            case_fig_2 <- case_fig_2 %>% add_lines(
+                x = ~date_choices[1:end_date_index_2], 
+                y = ~as.numeric(aggre_cases_log[input$country,])[1:end_date_index_2],
+                line = list(color = 'rgba(67,67,67,1)', width = 2),
+                name = 'Confirmed Cases'
+            )
+            
+            #add death line 
+            case_fig_2 <- case_fig_2 %>% add_lines(
+                x = ~date_choices[1:end_date_index_2],
+                y = ~as.numeric(aggre_death_log[input$country,])[1:end_date_index_2],
+                name = 'Death Toll'
+            )
+            
+            # set the axis for the plot
+            case_fig_2 <- case_fig_2 %>% layout(
+                title = paste0(input$country,'<br>','\t','Trends'),
+                xaxis = list(title = 'Date',showgrid = FALSE), 
+                yaxis = list(title = 'Comfirmed Cases/Deaths(Log Scale)', showgrid=FALSE)
+            )
+        }
+        
+        return(case_fig_2)
+    })
     
-    # tab panel 2 - Maps ----------------------------------------
+    
+    # tab panel 3 - Maps ----------------------------------------
     data_countries <- reactive({
         if(!is.null(input$choices)){
             if(input$choices == "Cases"){
