c6d98bc0a247037a19cc278c232f3f42d4f2fb35
Wendy Doan <doanthuolaf1@gmail.com>
2020-10-12 22:58:42 -0500

    update US state data

--- app/forge.R
+++ app/forge.R
@@ -45,10 +45,20 @@ if (!require("viridis")) {install.packages("viridis")
 #Data Sources
 "Dong E, Du H, Gardner L. An interactive web-based dashboard to track COVID-19 in real time. 
 Lancet Inf Dis. 20(5):533-534. doi: 10.1016/S1473-3099(20)30120-1"
+
+#From JH github
 #get the daily global cases data from API
 Cases_URL <- getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 global_cases <- read.csv(text = Cases_URL)
 
+#get the daily US cases
+CasesUS_URL<-getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
+US_cases<-read.csv(text=CasesUS_URL)
+
 #get the daily global deaths data from API
 Death_URL <- getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 global_death <- read.csv(text = Death_URL)
+
+#get the daily US deaths data
+DeathUS_URL<-getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv")
+US_death<-read.csv(text=DeathUS_URL)
