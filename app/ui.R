33aaf2efc0b584c1d94cb713496f9c156556b261
Minh D. Nguyen <ndminh1990@gmail.com>
2020-10-12 22:42:30 -0500

    update ui.R to include state tab

--- app/ui.R
+++ app/ui.R
@@ -27,8 +27,8 @@ shinyUI( navbarPage(
     collapsible = TRUE,
     theme = shinytheme("flatly"),
     
-    # tab panel 1 - Home ---------------------------------------------------
-    tabPanel("Home",icon = icon("home"),
+    # tab panel 1 - Global ---------------------------------------------------
+    tabPanel("Global",icon = icon("globe"),
         fluidPage( fluidRow(
             column(12, h1("Global Cases overview across time"),
                 
@@ -38,10 +38,10 @@ shinyUI( navbarPage(
                     column(6,
                         
                         # select date with slider
-                        sliderInput('date','Date Unitl:',
+                        sliderInput('date','Date Slider:',
                             min = as.Date(date_choices[1]),
                             max = as.Date(tail(date_choices,1)),
-                            value = as.Date(date_choices[1], '%Y-%m-%d'),
+                            value = as.Date(tail(date_choices,1)),
                             timeFormat = "%Y-%m-%d",
                             animate = TRUE, step = 5
                         ),
@@ -74,8 +74,56 @@ shinyUI( navbarPage(
     ),
     # end of tab panel 1 
 
+    # tab panel 2 - State ---------------------------------------------------
+    tabPanel("State",icon = icon("cog"),
+             fluidPage( fluidRow(
+                 column(12, h1("US Cases overview across time"),
+                        
+                        fluidRow(
+                            
+                            # date and country selections
+                            column(6,
+                                   
+                                   # select date with slider
+                                   sliderInput('date_2','Date Slider:',
+                                               min = as.Date(date_choices[1], '%Y-%m-%d'),
+                                               max = as.Date(tail(date_choices,1), '%Y-%m-%d'),
+                                               value = as.Date(date_choices[2], '%Y-%m-%d'),
+                                               timeFormat = "%Y-%m-%d",
+                                               animate = TRUE, step = 5
+                                   ),
+                                   
+                                   fluidRow(
+                                       # select country 
+                                       column(6, 
+                                              selectInput('state','Which State?',
+                                                          choices = country_names_choices,
+                                                          selected = 'United States of America')
+                                       ),
+                                       
+                                       # select log scale or not
+                                       column(6,
+                                              radioButtons("log_scale_2", "In Log Scale:",
+                                                           choices = c(TRUE,FALSE),
+                                                           selected = FALSE)
+                                       )
+                                   )
+                            ),
+                            
+                            
+                            # render plotly output
+                            column(width = 6,
+                                   plotlyOutput('case_overtime_state')
+                            )
+                        )
+                 )
+             ))
+    ),
+    # end of tab panel 2 
+        
+    
 
-    # tab panel 2 - Map ---------------------------------------------------
+    # tab panel 3 - Global Map ---------------------------------------------------
     tabPanel("Maps",icon = icon("map-marker-alt"), 
         div(class = 'outer',
             leafletOutput("map", width = "100%", height = "1200"),
@@ -111,7 +159,7 @@ shinyUI( navbarPage(
     # end of tab panel 2 
 
 
-    #tab panel 3 - Source ---------------------------------------------------
+    #tab panel 4 - Sources ---------------------------------------------------
     tabPanel("Data Source",icon = icon("cloud-download"),
         HTML(
             "
